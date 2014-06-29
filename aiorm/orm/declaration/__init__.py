bf6e3d88035c1615b6bf9fc096668629cf2328e1
Guillaume Gauvrit <guillaume@gauvr.it>
2014-06-29 21:50:23 +0200

    Move keys to columns

--- aiorm/orm/declaration/__init__.py
+++ aiorm/orm/declaration/__init__.py
@@ -1,6 +1,5 @@
 """ Declaration of SQL Schema """
 
-from .columns import Column
-from .keys import PrimaryKey, ForeignKey
+from .columns import Column, PrimaryKey, ForeignKey
 from .relations import OneToOne, OneToMany, ManyToMany
 from .types import Integer, Timestamp, Text, String
