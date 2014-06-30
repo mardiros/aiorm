d35a94bc25086cb51f431154f10b43f36da54a1d
Guillaume Gauvrit <guillaume@gauvr.it>
2014-06-30 21:18:15 +0200

    Add tests for operators

--- aiorm/orm/query/operators.py
+++ aiorm/orm/query/operators.py
@@ -5,7 +5,7 @@
 class and_:
 
     def __init__(self, *statements):
-        self.statements = statement
+        self.statements = statements
 
     def render_sql(self, renderer):
         return renderer.render_and(self)
@@ -26,37 +26,22 @@ class equal:
         return renderer.render_equal(self)
 
 
-class greater_than:
-    def __init__(self, column, value):
-        self.column = column
-        self.value = value
+class greater_than(equal):
 
     def render_sql(self, renderer):
         return renderer.render_greater_than(self)
 
 
-class greater_than_or_equal:
-    def __init__(self, column, value):
-        self.column = column
-        self.value = value
-
+class greater_than_or_equal(equal):
     def render_sql(self, renderer):
-        return renderer.render_greater_or_equal_than(self)
+        return renderer.render_greater_than_or_equal(self)
 
 
-class less_than:
-    def __init__(self, column, value):
-        self.column = column
-        self.value = value
-
+class less_than(equal):
     def render_sql(self, renderer):
         return renderer.render_less_than(self)
 
 
-class less_than_or_equal:
-    def __init__(self, column, value):
-        self.column = column
-        self.value = value
-
+class less_than_or_equal(equal):
     def render_sql(self, renderer):
-        return renderer.render_less_or_equal_than(self)
+        return renderer.render_less_than_or_equal(self)
---
+++ aiorm/tests/orm/query/test_operators.py
@@ -0,0 +1,74 @@
+from unittest.mock import Mock
+
+from aiorm.tests.testing import TestCase
+
+
+class OperatorTestCase(TestCase):
+
+    def test_and(self):
+        from aiorm.orm.query.operators import and_
+        visitor = Mock()
+        p1, p2, p3 = object(), object(), object()
+        stmt = and_(p1, p2, p3)
+        self.assertEqual(stmt.statements, (p1, p2, p3))
+        stmt.render_sql(visitor)
+        visitor.render_and.assert_called_with(stmt)
+
+    def test_or(self):
+        from aiorm.orm.query.operators import or_
+        visitor = Mock()
+        p1, p2, p3 = object(), object(), object()
+        stmt = or_(p1, p2, p3)
+        self.assertEqual(stmt.statements, (p1, p2, p3))
+        stmt.render_sql(visitor)
+        visitor.render_or.assert_called_with(stmt)
+
+    def test_equal(self):
+        from aiorm.orm.query.operators import equal
+        visitor = Mock()
+        column, value = Mock(), Mock()
+        stmt = equal(column, value)
+        self.assertEqual(stmt.column, column)
+        self.assertEqual(stmt.value, value)
+        stmt.render_sql(visitor)
+        visitor.render_equal.assert_called_with(stmt)
+
+    def test_greater_than(self):
+        from aiorm.orm.query.operators import greater_than
+        visitor = Mock()
+        column, value = Mock(), Mock()
+        stmt = greater_than(column, value)
+        self.assertEqual(stmt.column, column)
+        self.assertEqual(stmt.value, value)
+        stmt.render_sql(visitor)
+        visitor.render_greater_than.assert_called_with(stmt)
+
+    def test_greater_than_or_equal(self):
+        from aiorm.orm.query.operators import greater_than_or_equal
+        visitor = Mock()
+        column, value = Mock(), Mock()
+        stmt = greater_than_or_equal(column, value)
+        self.assertEqual(stmt.column, column)
+        self.assertEqual(stmt.value, value)
+        stmt.render_sql(visitor)
+        visitor.render_greater_than_or_equal.assert_called_with(stmt)
+
+    def test_less_than(self):
+        from aiorm.orm.query.operators import less_than
+        visitor = Mock()
+        column, value = Mock(), Mock()
+        stmt = less_than(column, value)
+        self.assertEqual(stmt.column, column)
+        self.assertEqual(stmt.value, value)
+        stmt.render_sql(visitor)
+        visitor.render_less_than.assert_called_with(stmt)
+
+    def test_less_than_or_equal(self):
+        from aiorm.orm.query.operators import less_than_or_equal
+        visitor = Mock()
+        column, value = Mock(), Mock()
+        stmt = less_than_or_equal(column, value)
+        self.assertEqual(stmt.column, column)
+        self.assertEqual(stmt.value, value)
+        stmt.render_sql(visitor)
+        visitor.render_less_than_or_equal.assert_called_with(stmt)
