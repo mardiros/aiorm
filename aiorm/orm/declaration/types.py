29c82e426ffa10e39088df666e1844fca1e8ca77
Guillaume Gauvrit <guillaume@gauvr.it>
2017-09-12 20:36:06 +0200

    Remove useless overriden methods

--- aiorm/orm/declaration/types.py
+++ aiorm/orm/declaration/types.py
@@ -62,17 +62,11 @@ class CIText(SQLType):
 
 class UUID(SQLType):
 
-    def __init__(self):
-        super().__init__()
-
     def render_sql(self, renderer):
         return renderer.render_uuid(self)
 
 
 class JSONB(SQLType):
-
-    def __init__(self):
-        super().__init__()
 
     def render_sql(self, renderer):
         return renderer.render_jsonb(self)
